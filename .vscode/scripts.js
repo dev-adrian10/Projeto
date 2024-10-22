a70e3127e4457e328288169f9b7429aa1b7432cc
Adrian Araujo <araujoadrian857@gmail.com>
2024-10-21 22:42:33 -0300

    :tada: initial commit

---
+++ .vscode/scripts.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+     //if(html.classList.contains('light')) {
+    //   html.classList.remove('light')
+    //}else {
+    // html.classList.add('light')
+    //}
+    const html = document.documentElement
+    html.classList.toggle("light")
+    
+    //pegar a tag img 
+    const img = document.querySelector("#profile img")
+
+    //substituir a imagem
+    if(html.classList.contains("light")) {
+        //se tiver light mode, adicionar imagem light
+        img.setAttribute("src", "/assets/avataradrilight.jpg")
+    } else {
+        //set tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "/assets/avataradrian.jpg")
+    }
+    }
